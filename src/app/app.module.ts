04a14301372929808bf8477b4c5ea94ef448c3cb
teeny909 <sk8leo@live.com>
2022-07-27 23:34:33 -0400

    added input component + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
