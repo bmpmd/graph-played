adb47684b661207e6574d9329d9e78715308659d
leo juarez <sk8leo@live.com>
2022-08-23 11:53:42 -0400

    using plotly js

--- src/app/components/out/out.component.ts
+++ src/app/components/out/out.component.ts
@@ -3,13 +3,30 @@ import { AppComponent } from 'src/app/app.component';
 @Component({
   selector: 'app-out',
   templateUrl: './out.component.html',
-  styleUrls: ['./out.component.css']
+  styleUrls: ['./out.component.css'],
 })
-export class OutComponent implements OnInit {
 
-  constructor(public appComponent: AppComponent) { }
+
+
+export class OutComponent implements OnInit {
+  public graph = {
+    data: [
+  
+      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+    ],
+    layout: { width: 320, height: 240, title: 'A Fancy Plot' }
+  };
+
+
+
+  constructor(public appComponent: AppComponent) {
+
+
+
+  }
 
   ngOnInit(): void {
   }
+
 
 }
--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -1,29 +1,37 @@
-export class Game {
-    appid: number;
-    name: string;
+interface GameResponse{
+    appid: number; 
+    name: string; 
     playtime_forever: number;
-    //imgUrl: string;
     playtime_windows_forever: number;
     playtimeMac: number;
     playtime_linux_forever: number;
     rtime_last_played: number;
+}
 
-    constructor(appid: number,
-        name: string,
-        playtime_forever: number,
-        playtime_windows_forever: number,
-        playtimeMac: number,
-        playtime_linux_forever: number,
-        rtime_last_played: number) {
-        this.appid = appid;
-        this.name = name;
-        this.playtime_forever = playtime_forever;
-        //this.imgUrl = `https://steamcdn-a.akamaihd.net/steam/apps/240/header.jpg`;
-        this.playtime_windows_forever = playtime_windows_forever;
-        this.playtimeMac = playtimeMac;
-        this.playtime_linux_forever = playtime_linux_forever;
-        this.rtime_last_played = rtime_last_played;
+export class Game {
+    appid: number;
+    name: string;
+    playtime_forever: number;
+    playtime_windows_forever: number;
+    playtimeMac: number;
+    playtime_linux_forever: number;
+    rtime_last_played: number;
+    
 
+    //additional props 
+    value: number;
+
+    constructor(rawResponse: GameResponse) {
+        this.appid = rawResponse.appid;
+        this.name = rawResponse.name;
+        this.playtime_forever = rawResponse.playtime_forever;
+        this.playtime_windows_forever = rawResponse.playtime_windows_forever;
+        this.playtimeMac = rawResponse.playtimeMac;
+        this.playtime_linux_forever = rawResponse.playtime_linux_forever;
+        this.rtime_last_played = rawResponse.rtime_last_played;
+
+        this.value = rawResponse.playtime_forever + rawResponse.playtime_windows_forever;
+        
 
     }
 }
