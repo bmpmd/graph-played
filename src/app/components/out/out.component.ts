e0c49f3baf1afa7ac53ce6b43c93d45c7f0545b5
leo juarez <sk8leo@live.com>
2022-08-24 15:08:18 -0400

    graph now loads without lag

--- src/app/components/out/out.component.ts
+++ src/app/components/out/out.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 import { GraphModel } from 'src/app/models/graphmodel';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+
 @Component({
   selector: 'app-out',
   templateUrl: './out.component.html',
@@ -8,10 +10,10 @@ import { GraphModel } from 'src/app/models/graphmodel';
 })
 
 
-
 export class OutComponent implements OnInit {
 
-
+  colorScheme: Color = { domain: ['#ef476f', '#ffd166', '#06d6a0', '#06d6a0', '#118ab2', '#073b4c'], group: ScaleType.Ordinal, selectable: true, name: 'Customer Usage', };
+  res: any[] = [];
   //options for ngx graph 
   view: any[] = [700, 370];
 
@@ -22,25 +24,23 @@ export class OutComponent implements OnInit {
   gradient: boolean = false;
   isDoughnut: boolean = true;
 
-  legendPosition: any = 'below';
-
-  colorScheme = {
-    domain: ['#704FC4', '#4B852C', '#B67A3D', '#5B6FC8', '#25706F']
-  };
 
 
+ 
 
-  constructor(public appComponent: AppComponent) { }
 
-  ngOnInit(): void {
+  constructor(public appComponent: AppComponent) { 
+   
   }
 
-  getModels(): any {
+  ngOnInit(): void {
+    //sets all the stuff right... 
     console.log("get models was")
+    this.appComponent.games = this.appComponent.games.sort(({playtime_forever:a}, {playtime_forever:b}) => b-a);
     var other: number = 0;
     var models = this.appComponent.games.map(({ name, playtime_forever }) => {
       var hrsApi: number = playtime_forever / 60;
-      if (hrsApi < 30) {
+      if (hrsApi < 5) {
         other += hrsApi;
         name = "Other"
         var value = parseFloat(hrsApi.toString()).toFixed(2);
@@ -50,14 +50,9 @@ export class OutComponent implements OnInit {
         var value = parseFloat(hrsApi.toString()).toFixed(2);
         return { name, value };
       }
+      
 
-
-      if (name !== "Other") {
-
-      }
-      else {
-        //console.log(`ignoring ${name} since it only has ${value} hrs.`)
-      }
+    
 
 
 
@@ -78,6 +73,63 @@ export class OutComponent implements OnInit {
     results.push(otherGames); 
     console.log("after::: ")
     console.log(results)
+    this.res = results;
+
+
+    //get top 3 
+    var topThreeRes = this.getTopThree();
+
+
+  }
+
+  //returns an array of image urls for the top three most played games 
+  //on a user's profile 
+  getTopThree():any{
+
+    var res = this.appComponent.games.sort(({playtime_forever:a}, {playtime_forever:b}) => b-a);
+    console.log(`top three: ${res[0].name}, ${res[1].name}, ${res[2].name} `)
+    return res;
+  }
+  getModels(): any {
+    console.log("get models was")
+    var other: number = 0;
+    this.appComponent.games = this.appComponent.games.sort(({playtime_forever:a}, {playtime_forever:b}) => b-a);
+    var models = this.appComponent.games.map(({ name, playtime_forever }) => {
+      var hrsApi: number = playtime_forever / 60;
+      if (hrsApi < 5) {
+        other += hrsApi;
+        name = "Other"
+        var value = parseFloat(hrsApi.toString()).toFixed(2);
+
+      }
+      else {
+        var value = parseFloat(hrsApi.toString()).toFixed(2);
+        return { name, value };
+      }
+
+
+    
+
+
+
+    });
+
+
+
+    console.log(`other hrs totla: ${other}`);
+    console.log("models below:::")
+    console.log(models)
+    const results = models.filter(element => {
+      return element !== undefined;
+    });
+
+    let otherGames =
+      { name: 'Other', value: other.toString() }
+
+    results.push(otherGames); 
+    console.log("after::: ")
+    console.log(results)
+    
     return results;
 
   }
